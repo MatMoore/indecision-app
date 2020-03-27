77b19b84ebb74a3f9224455d60211ffbd445d0fe
Mat Moore <MatMoore@users.noreply.github.com>
2020-03-27 16:53:49 +0000

    Using variables in JSX

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 console.log("App.js is running");
 
-var template = <h1>Hello world!!</h1>
+const app = {
+    title: "Hello App!",
+    subtitle: "This will solve all your problems",
+}
 
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+            <li>Item three</li>
+        </ol>
+    </div>
+)
+
+const user = {
+    name: "Mat",
+    age: 32,
+    location: "London"
+}
+
+const templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
 var appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
