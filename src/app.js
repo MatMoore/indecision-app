54e61664d0a31faf4922edd0ccc87d36af83a7c5
Mat Moore <MatMoore@users.noreply.github.com>
2020-04-24 15:52:50 +0100

    Use shorter syntax for setState calls

--- src/app.js
+++ src/app.js
@@ -16,11 +16,7 @@ class IndecisionApp extends React.Component {
     }
 
     handleDeleteOptions() {
-        this.setState(() => {
-            return {
-                options: []
-            }
-        })
+        this.setState(() => ({ options: [] }));
     }
 
     handleAddOption(option) {
@@ -30,11 +26,11 @@ class IndecisionApp extends React.Component {
             return 'This option already exists!'
         }
 
-        this.setState((prevState) => {
-            return {
+        this.setState((prevState) => (
+            {
                 options: prevState.options.concat([option])
             }
-        })
+        ));
     }
 
     render() {
@@ -126,10 +122,7 @@ class AddOption extends React.Component {
         
         const error = this.props.handleAddOption(option);
         
-        this.setState(() => {
-            return { error }
-        })
-
+        this.setState(() => ({error}));
     }
 
     render () {
