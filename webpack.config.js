d997f967bb8a126946ac54294b030ca2904645c5
Mat Moore <MatMoore@users.noreply.github.com>
2021-01-22 16:59:00 +0000

    Add SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,18 @@ module.exports = {
         filename: 'bundle.js'
     },
     'module': {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader', 'sass-loader']
+            }
+
+        ]
     },
     'devtool': 'cheap-module-eval-source-map',
     'devServer': {
