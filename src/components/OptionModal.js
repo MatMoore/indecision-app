e0def73b9d1e195ad295fcb6da61774466749cd3
Mat Moore <MatMoore@users.noreply.github.com>
2021-02-05 15:37:03 +0000

    Style the app

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
 const OptionModal = (props) => (
-    <Modal
-        isOpen={!!props.selectedOption}
-        contentLabel="SelectedOption"
-    >
-        <h3>SelectedOption</h3>
-        <p>{props.selectedOption}</p>
-        <button onClick={props.handleClose}>Close</button>
-    </Modal>
+  <Modal
+    isOpen={!!props.selectedOption}
+    contentLabel="SelectedOption"
+    closeTimeoutMS={200}
+    className="modal"
+  >
+    <h3 className="modal__title">SelectedOption</h3>
+    <p className="modal__body">{props.selectedOption}</p>
+    <button class="button" onClick={props.handleClose}>
+      Close
+    </button>
+  </Modal>
 );
 
 export default OptionModal;
